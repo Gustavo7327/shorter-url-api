365affb0e41fd2bf94ec9f67eea24880a179a36e
Gustavo Chaves <gustavosilva712493@gmail.com>
2024-07-16 10:59:45 -0300

    create controller

---
+++ src/main/java/com/gustavo7327/shorter_url/controller/URLController.java
@@ -0,0 +1,57 @@
+package com.gustavo7327.shorter_url.controller;
+
+import java.net.URI;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gustavo7327.shorter_url.dto.URLRequest;
+import com.gustavo7327.shorter_url.dto.URLResponse;
+import com.gustavo7327.shorter_url.entity.URLEntity;
+import com.gustavo7327.shorter_url.repository.URLRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class URLController {
+
+    private final URLRepository repository;
+    
+    public URLController(URLRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping(value = "/shorten-url")
+    public ResponseEntity<URLResponse> create(@RequestBody URLRequest request, HttpServletRequest httpServletRequest){
+
+        String id;
+        do{
+            id = RandomStringUtils.randomAlphanumeric(5,10);
+        } while(repository.existsById(id));
+
+        repository.save(new URLEntity(id, request.url()));
+        var redirect = httpServletRequest.getRequestURL().toString().replace("shorten-url",id);
+        return ResponseEntity.ok(new URLResponse(redirect));
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<Void> redirect(@PathVariable("id") String id){
+
+        var urlOp = repository.findById(id);
+
+        if(urlOp.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(urlOp.get().getUrlOriginal()));
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    }
+}
