54c953e14ada2bc7a4fa68f5bbc88995d073bda5
Gustavo Chaves <gustavosilva712493@gmail.com>
2024-07-15 23:11:57 -0300

    create repository

---
+++ src/main/java/com/gustavo7327/shorter_url/repository/URLRepository.java
@@ -0,0 +1,9 @@
+package com.gustavo7327.shorter_url.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gustavo7327.shorter_url.entity.URLEntity;
+
+public interface URLRepository extends JpaRepository<URLEntity,Long>{
+    
+}
