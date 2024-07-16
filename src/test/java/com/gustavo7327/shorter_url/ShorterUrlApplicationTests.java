c757e754373ff94b6541124cb13a0f2d500eef68
Gustavo Chaves <gustavosilva712493@gmail.com>
2024-07-15 22:17:46 -0300

    setup project

---
+++ src/test/java/com/gustavo7327/shorter_url/ShorterUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo7327.shorter_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorterUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
