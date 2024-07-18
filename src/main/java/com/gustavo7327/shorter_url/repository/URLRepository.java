ef09e96042f63e90a9476f89f2b0d22f51091a24
Gustavo Chaves <gustavosilva712493@gmail.com>
2024-07-18 01:16:38 -0300

    adding personalized query

--- src/main/java/com/gustavo7327/shorter_url/repository/URLRepository.java
+++ src/main/java/com/gustavo7327/shorter_url/repository/URLRepository.java
@@ -1,9 +1,12 @@
 package com.gustavo7327.shorter_url.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gustavo7327.shorter_url.entity.URLEntity;
 
 public interface URLRepository extends JpaRepository<URLEntity,String>{
     
+    Optional<URLEntity> findByUrlOriginal(String urlOriginal);
 }
