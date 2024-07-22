f4254bc30b6f9946197b5094b1b0ae42264ab499
Gustavo Chaves <gustavosilva712493@gmail.com>
2024-07-22 01:34:16 -0300

    adding icon to copy urls

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 
             success: function(response) {
                 $("#shortenedUrl").text("Acesse a URL em: " + response.url);
+                $("#shortenedUrl").append("<img src='imgs/file-copy-line.svg' alt='' width='20px' id='copy' onclick='copyUrl()'></img>");
                 $("#resultMessage").text("URL encurtada com sucesso!");
             },
             error: function(error) {
@@ -21,3 +22,11 @@ $(document).ready(function() {
         });
     });
 });
+
+function copyUrl(){
+    let pContent = $("#shortenedUrl").text();
+    let copy = pContent.substring(pContent.indexOf("http"));
+    navigator.clipboard.writeText(copy);
+    $("#shortenedUrl").find("#copy").remove();
+    $("#shortenedUrl").append("<img src='imgs/file-check-line.svg' alt='' width='20px' id='copy' onclick='copyUrl()'></img>");
+}
