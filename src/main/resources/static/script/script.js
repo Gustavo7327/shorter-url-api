ce4cfa8d20fb7fc1b8dbf4f8c3fe9c2ae8b0bbc1
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-01-12 17:42:18 -0300

    fix: bug in quick copy of url

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -12,8 +12,11 @@ $(document).ready(function() {
 
             success: function(response) {
                 $("#shortenedUrl").text("Acesse a URL em: " + response.url);
-                $("#shortenedUrl").append("<img src='imgs/file-copy-line.svg' alt='' width='20px' id='copy' onclick='copyUrl()'></img>");
+                $("#shortenedUrl").append("<img src='imgs/file-copy-line.svg' alt='' width='20px' id='copy'></img>");
                 $("#resultMessage").text("URL encurtada com sucesso!");
+                $("#shortenedUrl").on("click", "#copy", function() {
+                    copyUrl();
+                });                
             },
             error: function(error) {
                 $("#shortenedUrl").empty();
@@ -23,10 +26,20 @@ $(document).ready(function() {
     });
 });
 
-function copyUrl(){
+function copyUrl() {
     let pContent = $("#shortenedUrl").text();
     let copy = pContent.substring(pContent.indexOf("http"));
-    navigator.clipboard.writeText(copy);
+
+    let tempInput = document.createElement("textarea");
+    tempInput.value = copy;
+    document.body.appendChild(tempInput); 
+
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); 
+    document.execCommand("copy"); 
+
+    document.body.removeChild(tempInput);
+
     $("#shortenedUrl").find("#copy").remove();
-    $("#shortenedUrl").append("<img src='imgs/file-check-line.svg' alt='' width='20px' id='copy' onclick='copyUrl()'></img>");
+    $("#shortenedUrl").append("<img src='imgs/file-check-line.svg' alt='Copiado!' width='20px' id='copy' onclick='copyUrl()'></img>");
 }
